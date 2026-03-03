4db3d1d60cc40b7fd1d0f6b899fd89c36e7b7215
Blake Moody <32070003+blayyyyyk@users.noreply.github.com>
2026-03-03 14:41:36 -0500

    adjust desmume import in movie wrapper

--- gym_mkds/wrappers/movie_playback.py
+++ gym_mkds/wrappers/movie_playback.py
@@ -1,15 +1,15 @@
-from desmume.emulator import DeSmuME
+from desmume.emulator_mkds import MarioKart
 import gymnasium as gym
 from typing import Callable
 
 class MoviePlaybackWrapper(gym.Wrapper):
-    def __init__(self, env: gym.Env, path: str, func: Callable[[DeSmuME], bool]):
+    def __init__(self, env: gym.Env, path: str, func: Callable[[MarioKart], bool]):
         super(MoviePlaybackWrapper, self).__init__(env)
         assert self.has_wrapper_attr(
             "emu"
         ), "Provided environment does not have an emulator attribute. It is recommended to use the MarioKartEnv as your base environment."
         self.movie_path = path
-        self.emu: DeSmuME = self.get_wrapper_attr("emu")
+        self.emu: MarioKart = self.get_wrapper_attr("emu")
         self.func = func
 
     def reset(self, *, seed=None, options=None):
